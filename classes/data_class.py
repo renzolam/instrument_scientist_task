57b8f4781662980ca96e179e064a95405f740f7a
Renzo <paklam@bas.ac.uk>
2024-08-01 20:50:39 +0100

    adds vorticity measurement class

---
+++ classes/data_class.py
@@ -0,0 +1,45 @@
+import logging
+from datetime import datetime, timedelta, timezone
+
+from common_utils import log_utils
+from classes import main_runparams_cls
+
+logger = logging.getLogger(__name__)
+log_utils.set_logger(logger)
+
+
+class vort_measurement:
+
+    r1_b1: float
+    r1_b2: float
+    r2_b1: float
+    r2_b2: float
+    area: float
+    vorticity_mHz: float
+    MLT: float
+    geo_lat_c: float
+    geo_long_c: float
+    geo_lat_1: float
+    geo_long_1: float
+    geo_lat_2: float
+    geo_long_2: float
+    geo_lat_3: float
+    geo_long_3: float
+    geo_lat_4: float
+    geo_long_4: float
+    aacgm_lat_c: float
+    aacgm_long_c: float
+    aacgm_lat_1: float
+    aacgm_long_1: float
+    aacgm_lat_2: float
+    aacgm_long_2: float
+    aacgm_lat_3: float
+    aacgm_long_3: float
+    aacgm_lat_4: float
+    aacgm_long_4: float
+
+    def __init__(self, datapoint: dict):
+
+        for key, value in datapoint.items():
+            setattr(self, key, value)
+
